37b11040bf0279320838532ced14d11e7013dcb1
reinout <reinout@vanrees.org>
2010-06-12 16:45:26 +0000

    Added js for date popup

--- lizard_map/media/lizard_map/lizard_map.js
+++ lizard_map/media/lizard_map/lizard_map.js
@@ -56,10 +56,62 @@ setUpWorkspaceAcceptable = function() {
     });
 };
 
+// Date selector: only support for ONE date selector at the moment.
+
+function setUpDateChoice() {
+    $.datepicker.setDefaults($.datepicker.regional['nl']);
+    $("#id_date_start").datepicker();
+    $("#id_date_end").datepicker();
+}
+
+
+function setUpDateAjaxForm(overlay) {
+    var form = $("form", overlay);
+    form.submit(function() {
+        $.post(
+            form.attr('action'),
+            form.serialize(),
+            function(data) {
+                // This is the success handler.  Form can contain errors,
+                // though.
+                var newForm = $(data).find('form');
+                form.html(newForm);
+                setUpDateChoice();
+                var freshForm = $("form", overlay);
+                setUpDateAjaxForm(freshForm);
+                if (newForm.html().indexOf("rror") == -1) {
+                    // No error/Error, so refresh graphs and close the popup.
+                    loadCorrectlySizedImages();
+                    $("div.close", overlay).click();
+                }
+            });
+        return false;
+    });
+}
+
+
+function setUpDatePopup() {
+    $(".popup-trigger").live('mouseover', function() {
+        alert("init popup");
+        if (!$(this).data("popup-initialized")) {
+            $(this).data("popup-initialized", true);
+            $(this).overlay({
+                onLoad: function() {
+                    var overlay = this.getOverlay();
+                    setUpDateAjaxForm(overlay);
+                }
+            });
+
+        };
+    });
+};
+
 
 // Initialize all workspace actions.
 $(document).ready(function(){
     setUpWorkspaceAcceptable();
+    setUpDatePopup;
+    setUpDateChoice;
     /* Workspace functions, requires jquery.workspace.js */
     $(".workspace").workspaceInteraction();
     // $(".add-snippet").snippetInteraction(); // als het met live werkt kan het hier
