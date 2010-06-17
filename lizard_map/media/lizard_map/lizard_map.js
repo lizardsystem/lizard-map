4996726438e5e35524ef45c5e412483b570e9029
reinout <reinout@vanrees.org>
2010-06-17 14:04:41 +0000

    jslint

--- lizard_map/media/lizard_map/lizard_map.js
+++ lizard_map/media/lizard_map/lizard_map.js
@@ -1,28 +1,11 @@
-demo_map_click_handler = function(x, y, map) {
+// jslint configuration
+/*jslint browser: true */
+/*global $, OpenLayers, window, updateLayer, stretchOneSidebarBox */
 
-    var show_popup = function(data) {
-        popup = new OpenLayers.Popup("some_id",
-                                     new OpenLayers.LonLat(x,y),
-                                     new OpenLayers.Size(200,200),
-                                     data,
-                                     true);
-        popup.panMapIfOutOfView = true;
-        map.addPopup(popup);
-    };
-
-    var msg = ("You clicked near " + x + " N, " +
-               + y + " E");
-    show_popup(msg);
-
-    // $.get("/map/workspace/2/clickinfo/", { x: x, y: y },
-    //       show_popup
-    //      );
-};
-
-setUpWorkspaceAcceptable = function() {
+function setUpWorkspaceAcceptable() {
     // Set up draggability for current and future items.
     // See http://tinyurl.com/29lg4y3 .
-    $(".workspace-acceptable").live("mouseover", function() {
+    $(".workspace-acceptable").live("mouseover", function () {
         if (!$(this).data("draggable-initialized")) {
             $(this).data("draggable-initialized", true);
             $(this).draggable({
@@ -36,30 +19,32 @@ setUpWorkspaceAcceptable = function() {
         }
     });
     // Clicking a workspace-acceptable shows it in the 'temp' workspace.
-    $(".workspace-acceptable").live("click", function(event) {
+    $(".workspace-acceptable").live("click", function (event) {
+        var name, adapter_class, adapter_layer_json, url_add_item_temp;
         $(".workspace-acceptable").removeClass("selected");
         $(this).addClass("selected");
-        var name = $(this).attr("data-name");
-        var adapter_class = $(this).attr("data-adapter-class");
-        var adapter_layer_json = $(this).attr("data-adapter-layer-json");
-        var url_add_item_temp = $(".workspace").attr(
+        name = $(this).attr("data-name");
+        adapter_class = $(this).attr("data-adapter-class");
+        adapter_layer_json = $(this).attr("data-adapter-layer-json");
+        url_add_item_temp = $(".workspace").attr(
             "data-url-lizard-map-session-workspace-add-item-temp");
-        $.post(url_add_item_temp,
-               { name: name,
-                 adapter_class: adapter_class,
-                 adapter_layer_json: adapter_layer_json
-               },
-               function(workspace_id) {
-                   updateLayer(workspace_id);
-               });
+        $.post(
+            url_add_item_temp,
+            {name: name,
+             adapter_class: adapter_class,
+             adapter_layer_json: adapter_layer_json
+            },
+            function (workspace_id) {
+                updateLayer(workspace_id);
+            });
         stretchOneSidebarBox();
     });
-};
+}
 
 // Date selector: only support for ONE date selector at the moment.
 
 function setUpDateChoice() {
-    $.datepicker.setDefaults($.datepicker.regional['nl']);
+    $.datepicker.setDefaults($.datepicker.regional.nl);
     $("#id_date_start").datepicker();
     $("#id_date_end").datepicker();
 }
@@ -71,19 +56,20 @@ function reloadGraphs() {
 
 function setUpDateAjaxForm(overlay) {
     var form = $("form", overlay);
-    form.submit(function() {
+    form.submit(function () {
         $.post(
             form.attr('action'),
             form.serialize(),
-            function(data) {
+            function (data) {
                 // This is the success handler.  Form can contain errors,
                 // though.
-                var newForm = $(data).find('form');
+                var newForm, freshForm;
+                newForm = $(data).find('form');
                 form.html(newForm);
                 setUpDateChoice();
-                var freshForm = $("form", overlay);
+                freshForm = $("form", overlay);
                 setUpDateAjaxForm(freshForm);
-                if (newForm.html().indexOf("rror") == -1) {
+                if (newForm.html().indexOf("rror") === -1) {
                     // No error/Error, so refresh graphs and close the popup.
                     reloadGraphs();
                     $("div.close", overlay).click();
@@ -95,23 +81,22 @@ function setUpDateAjaxForm(overlay) {
 
 
 function setUpDatePopup() {
-    $(".popup-trigger").live('mouseover', function() {
+    $(".popup-trigger").live('mouseover', function () {
         if (!$(this).data("popup-initialized")) {
             $(this).data("popup-initialized", true);
             $(this).overlay({
-                onLoad: function() {
+                onLoad: function () {
                     var overlay = this.getOverlay();
                     setUpDateAjaxForm(overlay);
                 }
             });
-
-        };
+        }
     });
-};
+}
 
 
 // Initialize all workspace actions.
-$(document).ready(function(){
+$(document).ready(function () {
     setUpWorkspaceAcceptable();
     setUpDatePopup();
     setUpDateChoice();
