5ae3cf5da07ed8f5650fc9a1dce73be58bbba7d9
reinout <reinout@vanrees.org>
2010-06-22 11:53:09 +0000

    pep8

--- lizard_map/layers.py
+++ lizard_map/layers.py
@@ -6,6 +6,7 @@ import pkg_resources
 from lizard_map import coordinates
 from workspace import WorkspaceItemAdapter
 
+
 class WorkspaceItemAdapterShapefile(WorkspaceItemAdapter):
     """
     map layer
@@ -17,7 +18,7 @@ class WorkspaceItemAdapterShapefile(WorkspaceItemAdapter):
         """Return layer and styles for a shapefile.
 
         Registered as ``shapefile_layer``
-        
+
         http://127.0.0.1:8000/map/workspace/1/wms/?LAYERS=basic&SERVICE=WMS&VERSION=1.1.1&REQUEST=GetMap&STYLES=&EXCEPTIONS=application%2Fvnd.ogc.se_inimage&FORMAT=image%2Fjpeg&SRS=EPSG%3A900913&BBOX=523838.00391791,6818214.5267836,575010.91942212,6869720.7532931&WIDTH=140&HEIGHT=140
         """
         layers = []
@@ -28,7 +29,8 @@ class WorkspaceItemAdapterShapefile(WorkspaceItemAdapter):
             file=pkg_resources.resource_filename(
                 'lizard_map',
                 'test_shapefiles/KRWwaterlichamen_vlakken.shp'))
-        area_looks = mapnik.PolygonSymbolizer(mapnik.Color('#ffb975')) #light brownish
+        area_looks = mapnik.PolygonSymbolizer(mapnik.Color('#ffb975'))
+        # ^^^ light brownish
         line_looks = mapnik.LineSymbolizer(mapnik.Color('#dd0000'), 1)
         area_looks.fill_opacity = 0.5
         layout_rule = mapnik.Rule()
@@ -65,7 +67,7 @@ class WorkspaceItemAdapterShapefile(WorkspaceItemAdapter):
             name_in_shapefile = feature.properties['WGBNAAM']
             result.append({
                     'distance': 0.0,
-                    'name': name_in_shapefile
+                    'name': name_in_shapefile,
                     })
 
         return result
