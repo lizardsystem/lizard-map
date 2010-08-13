a940a976bcf59172469acdff94ab13d1eba2657f
jackha <jackha@gmail.com>
2010-08-13 10:02:47 +0000

    add adapter.value_aggregate

--- lizard_map/workspace.py
+++ lizard_map/workspace.py
@@ -181,6 +181,48 @@ class WorkspaceItemAdapter(object):
         """
         raise NotImplementedError
 
+    def value_aggregate(self, identifier, aggregate_functions,
+                        start_date, end_date):
+        """
+        Aggregates values according to aggregate_functions. See super
+        of this function.
+        """
+
+        result = {}
+        values = self.values(identifier, start_date, end_date)
+        values_only = [value['value'] for value in values]
+        values_only.sort()  # for percentile function
+        for key, value in aggregate_functions.items():
+            if key == 'min':
+                result_value = min(values_only)
+            elif key == 'max':
+                result_value = max(values_only)
+            elif key == 'avg':
+                result_value = float(sum(values_only)) / len(values_only)
+            elif key == 'count_lt':
+                if value is None:
+                    result_value = None
+                else:
+                    result_value = 0
+                    for v in values_only:
+                        if v < value:  # value is boundary value
+                            result_value += 1
+            elif key == 'count_gte':
+                if value is None:
+                    result_value = None
+                else:
+                    result_value = 0
+                    for v in values_only:
+                        if v >= value:  # value is boundary value
+                            result_value += 1
+            elif key == 'percentile':
+                rank = int(value * len(values_only) / 100.0 + 0.5)
+                result_value = values_only[rank]
+            else:
+                result_value = None
+            result[key] = result_value
+        return result
+
     def location(self, identifier=None, layout=None):
         """Return fews point representation corresponding to filter_id,
         location_id and parameter_id in same format as search function
