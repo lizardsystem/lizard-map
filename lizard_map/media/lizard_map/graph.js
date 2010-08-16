2a16d8d99c1dd59a979efc4f6132832461d10657
reinout <reinout@vanrees.org>
2010-08-16 09:17:23 +0000

    Added jslint configuration. No real problems found, but some cosmetics were changed.

--- lizard_map/media/lizard_map/graph.js
+++ lizard_map/media/lizard_map/graph.js
@@ -1,22 +1,27 @@
+// jslint configuration
+/*jslint browser: true */
+/*global $, reloadGraphs */
+
 /* Helper functions in graph edit screen.  Needs lizard.js in
 order to function correctly.  */
 function graph_save_snippet() {
     // the actual graph props are already stored in session on server
-    var url, workspace_item_id, workspace_item_location_identifier;
-    var workspace_item_location_shortname, workspace_item_location_name;
+    var url, workspace_item_id, workspace_item_location_identifier,
+        workspace_item_location_shortname, workspace_item_location_name;
     url = $(this).attr("data-url");
     workspace_item_id = $(this).attr("data-workspace-item-id");
     workspace_item_location_identifier = $(this).attr("data-workspace-item-location-identifier");
     workspace_item_location_shortname = $(this).attr("data-workspace-item-location-shortname");
     workspace_item_location_name = $(this).attr("data-workspace-item-location-name");
-    $.post(url,
-           {workspace_item_id: workspace_item_id,
-            workspace_item_location_identifier: workspace_item_location_identifier,
-            workspace_item_location_shortname: workspace_item_location_shortname,
-            workspace_item_location_name: workspace_item_location_name
-           }, function() {
-               reloadGraphs();
-           });
+    $.post(
+        url,
+        {workspace_item_id: workspace_item_id,
+         workspace_item_location_identifier: workspace_item_location_identifier,
+         workspace_item_location_shortname: workspace_item_location_shortname,
+         workspace_item_location_name: workspace_item_location_name
+        }, function () {
+            reloadGraphs();
+        });
 }
 
 function graph_action() {
@@ -24,12 +29,13 @@ function graph_action() {
     var $form, url;
     $form = $(this).parents("form.graph-options");
     url = $form.attr("data-url");
-    $.post(url,
-           $form.serialize(),
-           function() {
-               reloadGraphs();
-               $("div.close").click();
-           });
+    $.post(
+        url,
+        $form.serialize(),
+        function () {
+            reloadGraphs();
+            $("div.close").click();
+        });
 }
 
 $(document).ready(function () {
