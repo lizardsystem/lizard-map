55f0880d0f1aec944786495a0392f53a2a05425b
jackha <jackha@gmail.com>
2010-09-10 12:06:52 +0000

    jslint

--- lizard_map/media/lizard_map/lizard_wms.js
+++ lizard_map/media/lizard_map/lizard_wms.js
@@ -11,7 +11,7 @@ function updateLayer(workspace_id) {
 
 function updateLayers() {
     var i;
-    for (i=0; i<layers.length; i++) {
+    for (i = 0; i < layers.length; i++) {
         if (layers[i] !== undefined) {
             updateLayer(i);
         }
@@ -20,8 +20,10 @@ function updateLayers() {
 
 
 function showMap() {
-    var options, openstreetmapLayer, MapClickControl, map_click_control, map_hover_control;
-    var javascript_click_handler_name, javascript_hover_handler_name, $lizard_map_wms;
+    var options, openstreetmapLayer, MapClickControl,
+        map_click_control, map_hover_control,
+        javascript_click_handler_name, javascript_hover_handler_name,
+        $lizard_map_wms;
 
     // Find client-side extra data.
     $lizard_map_wms = $("#lizard-map-wms");
@@ -32,9 +34,7 @@ function showMap() {
         displayProjection: new OpenLayers.Projection("EPSG:4326"),
         units: "m",
         numZoomLevels: 18,
-        //maxExtent: new OpenLayers.Bounds(-20037508, -20037508,
-        //                                 20037508, 20037508.34),
-        maxExtent: new OpenLayers.Bounds(129394,6659216,1335570,7306790)
+        maxExtent: new OpenLayers.Bounds(129394, 6659216, 1335570, 7306790)
     };
     map = new OpenLayers.Map('map', options);
 
@@ -46,7 +46,7 @@ function showMap() {
     map.addLayer(openstreetmapLayer);
 
     // Add our own data layers.
-    $(".workspace-layer").each(function() {
+    $(".workspace-layer").each(function () {
         var workspace_id, workspace_name, workspace_wms;
         workspace_id = $(this).attr("data-workspace-id");
         workspace_name = $(this).attr("data-workspace-name");
@@ -64,80 +64,80 @@ function showMap() {
     map.addControl(new OpenLayers.Control.LayerSwitcher({'ascending': false}));
     // Click handling.
     javascript_click_handler_name = $lizard_map_wms.attr("data-javascript-click-handler");
-    if (javascript_click_handler_name != undefined) {
-      MapClickControl = OpenLayers.Class(OpenLayers.Control, {
-          defaultHandlerOptions: {
-              'single': true,
-              'double': false,
-              'pixelTolerance': 0,
-              'stopSingle': false,
-              'stopDouble': false
-          },
+    if (javascript_click_handler_name !== undefined) {
+        MapClickControl = OpenLayers.Class(OpenLayers.Control, {
+            defaultHandlerOptions: {
+                'single': true,
+                'double': false,
+                'pixelTolerance': 0,
+                'stopSingle': false,
+                'stopDouble': false
+            },
 
-          initialize: function(options) {
-              this.handlerOptions = OpenLayers.Util.extend(
-                  {}, this.defaultHandlerOptions
-              );
-              OpenLayers.Control.prototype.initialize.apply(
-                  this, arguments
-              );
-              this.handler = new OpenLayers.Handler.Click(
-                  this, {
-                      'click': this.trigger
-                  }, this.handlerOptions
-              );
-          },
+            initialize: function (options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                    {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                    this, arguments
+                );
+                this.handler = new OpenLayers.Handler.Click(
+                    this, {
+                        'click': this.trigger
+                    }, this.handlerOptions
+                );
+            },
 
-          trigger: function(e) {
-              var lonlat;
-              lonlat = map.getLonLatFromViewPortPx(e.xy);
-              eval(javascript_click_handler_name)(lonlat.lon, lonlat.lat, map);
-          }
-      });
-      map_click_control = new MapClickControl();
-      map.addControl(map_click_control);
-      map_click_control.activate();
+            trigger: function (e) {
+                var lonlat;
+                lonlat = map.getLonLatFromViewPortPx(e.xy);
+                eval(javascript_click_handler_name)(lonlat.lon, lonlat.lat, map);
+            }
+        });
+        map_click_control = new MapClickControl();
+        map.addControl(map_click_control);
+        map_click_control.activate();
     }
     // Hover handling.
     javascript_hover_handler_name = $lizard_map_wms.attr("data-javascript-click-handler");
-    if (javascript_hover_handler_name != undefined) {
-      // Example code from
-      // http://trac.openlayers.org/browser/trunk/openlayers/examples/hover-handler.html
-      MapHoverControl = OpenLayers.Class(OpenLayers.Control, {
-          defaultHandlerOptions: {
-              'delay': 500,
-              'pixelTolerance': null,
-              'stopMove': false
-          },
+    if (javascript_hover_handler_name !== undefined) {
+        // Example code from
+        // http://trac.openlayers.org/browser/trunk/openlayers/examples/hover-handler.html
+        MapHoverControl = OpenLayers.Class(OpenLayers.Control, {
+            defaultHandlerOptions: {
+                'delay': 500,
+                'pixelTolerance': null,
+                'stopMove': false
+            },
 
-          initialize: function(options) {
-              this.handlerOptions = OpenLayers.Util.extend(
-                  {}, this.defaultHandlerOptions
-              );
-              OpenLayers.Control.prototype.initialize.apply(
-                  this, arguments
-              );
-              this.handler = new OpenLayers.Handler.Hover(
-                  this,
-                  {'pause': this.onPause, 'move': this.onMove},
-                  this.handlerOptions
-              );
-          },
+            initialize: function (options) {
+                this.handlerOptions = OpenLayers.Util.extend(
+                    {}, this.defaultHandlerOptions
+                );
+                OpenLayers.Control.prototype.initialize.apply(
+                    this, arguments
+                );
+                this.handler = new OpenLayers.Handler.Hover(
+                    this,
+                    {'pause': this.onPause, 'move': this.onMove},
+                    this.handlerOptions
+                );
+            },
 
-          onPause: function(e) {
-              var lonlat;
-              lonlat = map.getLonLatFromViewPortPx(e.xy);
-              eval(javascript_hover_handler_name)(lonlat.lon, lonlat.lat, map);
-          },
+            onPause: function (e) {
+                var lonlat;
+                lonlat = map.getLonLatFromViewPortPx(e.xy);
+                eval(javascript_hover_handler_name)(lonlat.lon, lonlat.lat, map);
+            },
 
-          onMove: function(evt) {
-              $("#hover-popup").remove();
-          }
-      });
+            onMove: function (evt) {
+                $("#hover-popup").remove();
+            }
+        });
 
-      map_hover_control = new MapHoverControl();
-      map.addControl(map_hover_control);
-      map_hover_control.activate();
+        map_hover_control = new MapHoverControl();
+        map.addControl(map_hover_control);
+        map_hover_control.activate();
     }
     // Extend zooming.
     map.setCenter(new OpenLayers.LonLat(550000, 6850000), 10);
