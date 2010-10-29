58a656cb226c68012883a815390ee88e08bbde2e
reinout <reinout@vanrees.org>
2010-10-29 09:46:03 +0000

    pep8

--- lizard_map/mapnik_helper.py
+++ lizard_map/mapnik_helper.py
@@ -1,8 +1,12 @@
 """
 Mapnik helper functions
 """
+import logging
+
 import mapnik
 from django.conf import settings
+
+logger = logging.getLogger(__name__)
 
 
 def database_settings(name="default", user_settings=None):
@@ -24,7 +28,8 @@ def database_settings(name="default", user_settings=None):
         if name in user_settings.DATABASES:
             database_settings = user_settings.DATABASES[name]
             if database_settings['ENGINE'] in (
-                'postgresql_psycopg2', 'django.contrib.gis.db.backends.postgis'):
+                'postgresql_psycopg2',
+                'django.contrib.gis.db.backends.postgis'):
                 datasource = mapnik.PostGIS
                 options = {
                     'host': database_settings["HOST"],
@@ -33,7 +38,8 @@ def database_settings(name="default", user_settings=None):
                     'dbname': database_settings["NAME"]}
             else:
                 raise RuntimeError(
-                    'Sorry, unconfigured db engine (%s, name=%s) for mapnik integration.' % (
+                    'Sorry, unconfigured db engine (%s, name=%s) for '
+                    'mapnik integration.' % (
                         database_settings["ENGINE"], name))
         else:
             raise RuntimeError(
@@ -59,11 +65,11 @@ def database_settings(name="default", user_settings=None):
         raise RuntimeError(
             'Sorry, unconfigured db (%s and %s) for mapnik integration.' % (
                 user_settings.DATABASE_ENGINE, user_settings.DATABASES))
-
     return datasource, options
 
 
-def create_layer_from_query(query, projection, geometry_field=None, srid=None, user_settings=None):
+def create_layer_from_query(query, projection, geometry_field=None, srid=None,
+                            user_settings=None):
     """Return layer for the given table_view and projection."""
 
     layer = mapnik.Layer('Geometry from DB', projection)
@@ -72,13 +78,10 @@ def create_layer_from_query(query, projection, geometry_field=None, srid=None, u
         user_settings = settings
 
     datasource, options = database_settings(user_settings=user_settings)
-
     if geometry_field is not None:
         options['geometry_field'] = geometry_field
     if srid is not None:
         options['srid'] = srid
     layer.datasource = datasource(table=str(query), **options)
-
-    print query
 
     return layer
