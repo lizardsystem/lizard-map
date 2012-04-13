a965ad671d29fb1ecdb5bbe6b0458f82fc4c62ad
Reinout van Rees <reinout@vanrees.org>
2012-04-13 14:57:21 +0200

    added ADMIN_MEDIA_PREFIX = STATIC_URL so that we can see the admin css

--- lizard_map/testsettings.py
+++ lizard_map/testsettings.py
@@ -62,6 +62,7 @@ MEDIA_ROOT = os.path.join(BUILDOUT_DIR, 'var', 'media')
 STATIC_ROOT = os.path.join(BUILDOUT_DIR, 'var', 'static')
 MEDIA_URL = '/media/'
 STATIC_URL = '/static_media/'
+ADMIN_MEDIA_PREFIX = STATIC_URL + 'admin/'
 
 LOGGING = setup_logging(BUILDOUT_DIR)
 
