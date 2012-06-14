5975d14acf9c42d92e91f138fa57e7eddc9f4577
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-06-14 14:59:16 +0200

    Add moving box on collage-add.

--- lizard_map/static/lizard_map/lizard_map.js
+++ lizard_map/static/lizard_map/lizard_map.js
@@ -731,6 +731,26 @@ function actionPostEditCollageItem(event) {
 /* click on collage-add item */
 function actionPostCollageAdd(event) {
     var $target, adapter_class, name, adapter_layer_json, identifier;
+    /* New bootstrap-era interaction */
+   {
+        var $layer_button, $moving_box, move_down, move_right;
+        $layer_button = $(".secondary-sidebar-button");
+        $("#page").after('<div id="moving-box">');
+        $moving_box = $("#moving-box");
+        $moving_box.offset($(this).offset());
+        $moving_box.width($(this).width());
+        $moving_box.height($(this).height());
+        move_down = $layer_button.offset().top - $(this).offset().top;
+        move_right = $layer_button.offset().left - $(this).offset().left;
+        $moving_box.animate({
+            left: '+=' + move_right,
+            top: '+=' + move_down,
+            width: $layer_button.width(),
+            height: $layer_button.height()
+            }, 1000, function() {
+                $moving_box.remove();
+            });
+    }
     $target = $(event.target);
     name = $target.attr("data-name");
     adapter_class = $target.attr("data-adapter-class");
