603b8bc56afbe1b37a69dce2cbc59da0ed23418f
ejnens <erikjan.vos@nelen-schuurmans.nl>
2012-09-24 02:16:32 +0200

    disabled lessc, css is now in lizard_ui

--- make_less_and_coffee.sh
+++ make_less_and_coffee.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-lessc lizard_map/static/lizard_map/lizard_map.less lizard_map/static/lizard_map/lizard_map.css
+#lessc lizard_map/static/lizard_map/lizard_map.less lizard_map/static/lizard_map/lizard_map.css
+echo "No less files anymore, nothing to do"
