eca0d94303c3d3097f3b6cb4c851b77932c1cfdb
Gijs Nijholt <gijs.nijholt@gmail.com>
2012-11-13 16:11:30 +0100

    Enabled CQL filtering on all years and scenarios for both maps.Refactored the layer redrawing function while I was at it.

--- lizard_map/static/lizard_map/lizard_compare.js
+++ lizard_map/static/lizard_map/lizard_compare.js
@@ -19,13 +19,15 @@ $(document).keyup(function(e) {
 });
 
 $(document).ready(function() {
-    $('#map-b').data('wms-cql-filters', {'year':2050});
-    $('#map-b').data('wms-cql-filters', {'year':2050});
+    // Initialize CQL filters
+    $('#map-a').data('wms-cql-filters', {'year':'2015', 'scenario': '\'rust\''});
+    $('#map-b').data('wms-cql-filters', {'year':'2015', 'scenario': '\'rust\''});
 
-    // Resize map viewports on initial page load.
+    // Resize map viewports on initial page load
     $('#map-a').css('height',  ($("#content").height()/2) + 'px');
     $('#map-b').css('height',  ($("#content").height()/2) + 'px');
 
+    // Resize menu wrappers to half screen size
     $('#mapawrapper').css('height',  ($("#content").height()/2) - 74 + 'px');
     $('#mapbwrapper').css('height',  ($("#content").height()/2) - 74 + 'px');
 
@@ -68,18 +70,25 @@ $(document).ready(function() {
     window.mapa = mapa;
     window.mapb = mapb;
 
-    // Toggle the year controls and set the CQL filter data attributes
-    function updateMapAYearButtons(year) {
-        $('#mapa-controls #year-2100').toggleClass('active');
-        $('#mapa-controls #year-2050').toggleClass('active');
-        $('#map-a').data('wms-cql-filters', {'year':year});
+
+    // CQL variable updating per map
+    function updateCQLA(type, value) {
+        var cql_filters = $('#map-a').data('wms-cql-filters');
+        cql_filters[type] = value;
+        $('#map-a').data('wms-cql-filters', cql_filters);
+        console.log($('#map-a').data('wms-cql-filters'));
+        return true;
     }
 
-    function updateMapBYearButtons(year) {
-        $('#mapb-controls #year-2100').toggleClass('active');
-        $('#mapb-controls #year-2050').toggleClass('active');
-        $('#map-b').data('wms-cql-filters', {'year':year});
+    function updateCQLB(type, value) {
+        var cql_filters = $('#map-b').data('wms-cql-filters');
+        cql_filters[type] = value;
+        $('#map-b').data('wms-cql-filters', cql_filters);
+        console.log($('#map-b').data('wms-cql-filters'));
+        return true;
     }
+
+
 
     // Set the locations and add the baselayer for both maps
     mapa.setView(new L.LatLng(lat, lng), z).addLayer(baseLayerA);
@@ -112,198 +121,248 @@ $(document).ready(function() {
 
 
 
-      $('#mapa-controls #year-2100').click(function() {
-        // Set the year to 2100 for map A
-        updateMapAYearButtons('2100');
+
+    function reloadALayers(self, key, value) {
+        // Remove the layer
+        mapa.removeLayer(mapaLayers[self.attr('id')]);
+
+        var layer_filters = self.data("workspace-wms-cql-filters");
+        var selected_filters = $('#map-a').data('wms-cql-filters');
+
+        var cql_filters = '';
+
+        // Add the filters that are selected and available for this layer.
+        for (key in selected_filters){
+            if ($.inArray(key, layer_filters) !== -1){
+                cql_filters += key + '=' + selected_filters[key];
+            }
+        }
+
+        // params = $.parseJSON($(this).data("params"));
+        params = $.parseJSON(self.attr("data-params"));
+        // Important: data() did not work here ^^^, attr() does!
+
+        url = self.data("url");
+
+        if(params && params.layers) {
+
+            opts = {
+                layers: params.layers,
+                format: 'image/png',
+                transparent: true,
+                reuseTiles: true
+            };
+            
+            if(cql_filters) {
+                opts['cql_filter'] = cql_filters;
+            }
+
+            mapaLayers[self.attr('id')] = L.tileLayer.wms(url, opts);
+
+            // Add the new layer
+            mapa.addLayer(mapaLayers[self.attr('id')]);
+        }
+    }
+
+
+    function reloadBLayers(self, key, value) {
+        // Remove the layer
+        mapb.removeLayer(mapbLayers[self.attr('id')]);
+
+        var layer_filters = self.data("workspace-wms-cql-filters");
+        var selected_filters = $('#map-b').data('wms-cql-filters');
+
+        var cql_filters = '';
+
+        // Add the filters that are selected and available for this layer.
+        for (key in selected_filters){
+            if ($.inArray(key, layer_filters) !== -1){
+                cql_filters += key + '=' + selected_filters[key];
+            }
+        }
+
+        // params = $.parseJSON($(this).data("params"));
+        params = $.parseJSON(self.attr("data-params"));
+        // Important: data() did not work here ^^^, attr() does!
+
+        url = self.data("url");
+
+        if(params && params.layers) {
+
+            opts = {
+                layers: params.layers,
+                format: 'image/png',
+                transparent: true,
+                reuseTiles: true
+            };
+            
+            if(cql_filters) {
+                opts['cql_filter'] = cql_filters;
+            }
+
+            mapbLayers[self.attr('id')] = L.tileLayer.wms(url, opts);
+
+            // Add the new layer
+            mapb.addLayer(mapbLayers[self.attr('id')]);
+        }
+    }
+
+
+
+    // MAP A CONTROL HANDLERS
+    $('#mapa-controls #scenario-rust').click(function() {
+        // Set the scenario to rust for map A
+        updateCQLA('scenario', "'rust'");
 
         // For every checked workspace item
         $(".ws-a-item:checked").each(function(key, value) {
+            reloadALayers($(this), key, value);
+        });
+      });
 
-            // Remove the layer
-            mapa.removeLayer(mapaLayers[$(this).attr('id')]);
 
-            var layer_filters = $(this).data("workspace-wms-cql-filters");
-            var selected_filters = $('#map-a').data('wms-cql-filters');
+    $('#mapa-controls #scenario-druk').click(function() {
+        // Set the scenario to druk for map A
+        updateCQLA('scenario', "'druk'");
 
-            var cql_filters = '';
+        // For every checked workspace item
+        $(".ws-a-item:checked").each(function(key, value) {
+            reloadALayers($(this), key, value);
+        });
+      });
 
-            // Add the filters that are selected and available for this layer.
-            for (key in selected_filters){
-                if ($.inArray(key, layer_filters) !== -1){
-                    cql_filters += key + '=' + selected_filters[key];
-                }
-            }
 
-            // params = $.parseJSON($(this).data("params"));
-            params = $.parseJSON($(this).attr("data-params"));
-            // Important: data() did not work here ^^^, attr() does!
+    $('#mapa-controls #scenario-stoom').click(function() {
+        // Set the scenario to stoom for map A
+        updateCQLA('scenario', "'stoom'");
 
-            url = $(this).data("url");
+        // For every checked workspace item
+        $(".ws-a-item:checked").each(function(key, value) {
+            reloadALayers($(this), key, value);
+        });
+      });
 
-            if(params && params.layers) {
+    $('#mapa-controls #scenario-warm').click(function() {
+        // Set the scenario to warm for map A
+        updateCQLA('scenario', "'warm'");
 
-                opts = {
-                    layers: params.layers,
-                    format: 'image/png',
-                    transparent: true,
-                    reuseTiles: true
-                };
-                
-                if(cql_filters) {
-                    opts['cql_filter'] = cql_filters;
-                }
+        // For every checked workspace item
+        $(".ws-a-item:checked").each(function(key, value) {
+            reloadALayers($(this), key, value);
+        });
+      });
 
-                mapaLayers[$(this).attr('id')] = L.tileLayer.wms(url, opts);
+      $('#mapa-controls #year-2015').click(function() {
+        // Set the year to 2015 for map A
+        updateCQLA('year', '2015');
 
-                // Add the new layer
-                mapa.addLayer(mapaLayers[$(this).attr('id')]);
-            }
+        // For every checked workspace item
+        $(".ws-a-item:checked").each(function(key, value) {
+            reloadALayers($(this), key, value);
+        });
+        
+      });
+
+
+      $('#mapa-controls #year-2100').click(function() {
+        // Set the year to 2100 for map A
+        updateCQLA('year', '2100');
+
+        // For every checked workspace item
+        $(".ws-a-item:checked").each(function(key, value) {
+            reloadALayers($(this), key, value);
         });
         
       });
 
       $('#mapa-controls #year-2050').click(function() {
         // Set the year to 2050 for map A
-        updateMapAYearButtons('2050');
+        updateCQLA('year', '2050');
 
         // For every checked workspace item
         $(".ws-a-item:checked").each(function(key, value) {
-
-            // Remove the layer
-            mapa.removeLayer(mapaLayers[$(this).attr('id')]);
-
-            var layer_filters = $(this).data("workspace-wms-cql-filters");
-            var selected_filters = $('#map-a').data('wms-cql-filters');
-
-            var cql_filters = '';
-
-            // Add the filters that are selected and available for this layer.
-            for (key in selected_filters){
-                if ($.inArray(key, layer_filters) !== -1){
-                    cql_filters += key + '=' + selected_filters[key];
-                }
-            }
-
-            // params = $.parseJSON($(this).data("params"));
-            params = $.parseJSON($(this).attr("data-params"));
-
-            url = $(this).data("url");
-
-            if(params && params.layers) {
-
-                opts = {
-                    layers: params.layers,
-                    format: 'image/png',
-                    transparent: true,
-                    reuseTiles: true
-                };
-                
-                if(cql_filters) {
-                    opts['cql_filter'] = cql_filters;
-                }
-
-                mapaLayers[$(this).attr('id')] = L.tileLayer.wms(url, opts);
-
-                // Add the new layer
-                mapa.addLayer(mapaLayers[$(this).attr('id')]);
-            }
+            reloadALayers($(this), key, value);
         });
       });
 
+    // MAP B CONTROL HANDLERS
+    $('#mapb-controls #scenario-rust').click(function() {
+        // Set the scenario to rust for map B
+
+        updateCQLB('scenario', 'rust');
+
+        // For every checked workspace item
+        $(".ws-b-item:checked").each(function(key, value) {
+            reloadBLayers($(this), key, value);
+        });
+      });
+
+
+    $('#mapb-controls #scenario-druk').click(function() {
+        // Set the scenario to druk for map B
+
+        updateCQLB('scenario', 'druk');
+
+        // For every checked workspace item
+        $(".ws-b-item:checked").each(function(key, value) {
+            reloadBLayers($(this), key, value);
+        });
+      });
+
+
+    $('#mapb-controls #scenario-stoom').click(function() {
+        // Set the scenario to stoom for map B
+
+        updateCQLB('scenario', 'stoom');
+
+        // For every checked workspace item
+        $(".ws-b-item:checked").each(function(key, value) {
+            reloadBLayers($(this), key, value);
+        });
+      });
+
+
+    $('#mapb-controls #scenario-warm').click(function() {
+        // Set the scenario to warm for map B
+
+        updateCQLB('scenario', 'warm');
+
+        // For every checked workspace item
+        $(".ws-b-item:checked").each(function(key, value) {
+            reloadBLayers($(this), key, value);
+        });
+      });
+
+
+      $('#mapb-controls #year-2015').click(function() {
+        // Set the year to 2015 for map B
+
+        updateCQLB('year', '2015');
+
+        // For every checked workspace item
+        $(".ws-b-item:checked").each(function(key, value) {
+            reloadBLayers($(this), key, value);
+        });
+      });
 
 
       $('#mapb-controls #year-2100').click(function() {
         // Set the year to 2100 for map B
-        updateMapBYearButtons('2100');
+        updateCQLB('year', '2100');
 
         // For every checked workspace item
         $(".ws-b-item:checked").each(function(key, value) {
-
-            // Remove the layer
-            mapb.removeLayer(mapbLayers[$(this).attr('id')]);
-
-            var layer_filters = $(this).data("workspace-wms-cql-filters");
-            var selected_filters = $('#map-b').data('wms-cql-filters');
-
-            var cql_filters = '';
-
-            // Add the filters that are selected and available for this layer.
-            for (key in selected_filters){
-                if ($.inArray(key, layer_filters) !== -1){
-                    cql_filters += key + '=' + selected_filters[key];
-                }
-            }
-
-            params = $.parseJSON($(this).attr("data-params"));
-
-            url = $(this).data("url");
-
-            if(params && params.layers) {
-
-                opts = {
-                    layers: params.layers,
-                    format: 'image/png',
-                    transparent: true,
-                    reuseTiles: true
-                };
-                
-                if(cql_filters) {
-                    opts['cql_filter'] = cql_filters;
-                }
-
-                mapbLayers[$(this).attr('id')] = L.tileLayer.wms(url, opts);
-
-                // Add the new layer
-                mapb.addLayer(mapbLayers[$(this).attr('id')]);
-            }
+            reloadBLayers($(this), key, value);
         });
-        
       });
 
       $('#mapb-controls #year-2050').click(function() {
         // Set the year to 2050 for map B
-        updateMapBYearButtons('2050');
+        updateCQLB('year', '2050');
 
         // For every checked workspace item
         $(".ws-b-item:checked").each(function(key, value) {
-
-            // Remove the layer
-            mapb.removeLayer(mapbLayers[$(this).attr('id')]);
-
-            var layer_filters = $(this).data("workspace-wms-cql-filters");
-            var selected_filters = $('#map-b').data('wms-cql-filters');
-
-            var cql_filters = '';
-
-            // Add the filters that are selected and available for this layer.
-            for (key in selected_filters){
-                if ($.inArray(key, layer_filters) !== -1){
-                    cql_filters += key + '=' + selected_filters[key];
-                }
-            }
-
-            params = $.parseJSON($(this).attr("data-params"));
-
-            url = $(this).data("url");
-
-            if(params && params.layers) {
-
-                opts = {
-                    layers: params.layers,
-                    format: 'image/png',
-                    transparent: true,
-                    reuseTiles: true
-                };
-                
-                if(cql_filters) {
-                    opts['cql_filter'] = cql_filters;
-                }
-
-                mapbLayers[$(this).attr('id')] = L.tileLayer.wms(url, opts);
-
-                // Add the new layer
-                mapb.addLayer(mapbLayers[$(this).attr('id')]);
-            }
+            reloadBLayers($(this), key, value);
         });
       });
 
@@ -357,8 +416,6 @@ $(document).ready(function() {
 
             var layer_filters = $(this).data("workspace-wms-cql-filters");
             var selected_filters = $('#map-b').data('wms-cql-filters');
-
-            console.log(selected_filters);
             
             var cql_filters = '';
 
