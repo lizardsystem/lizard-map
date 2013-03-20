39920ed195107af76dd87384d36e211ac0d97a13
Jack Ha <jackha@gmail.com>
2013-03-20 12:12:12 +0100

    Update animations in workspace screen

--- lizard_map/static/lizard_map/animations.js
+++ lizard_map/static/lizard_map/animations.js
@@ -312,20 +312,19 @@ function init_animation() {
 
 
 function init_control_panel() {
-    //$('#controlpanel').off('click', '.btn-start-stop');
-    //#('#controlpanel').off('click', '.btn-reset');
-
-
+  //$('#controlpanel').off('click', '.btn-start-stop');
+  //#('#controlpanel').off('click', '.btn-reset');
+  
   // Bind the control panel to the view.
-    control_panel_view = new ControlPanelView({el: $('#controlpanel')});
-    //console.log('control panel view ', control_panel_view);
+  control_panel_view = new ControlPanelView({el: $('#controlpanel')});
+  //console.log('control panel view ', control_panel_view);
   //control_panel_view = new ControlPanelView({el: $('#controlpanel2')});
 }
 
 $(document).ready(function() {
-   //init_animation();
-   init_control_panel();
+  //init_animation();
+  init_control_panel();
   // special for the demo march 2013
-  $("a.popup-date-range").parent().css("display", "none");
-  $("a.map-multiple-selection").parent().css("display", "none");
+  //$("a.popup-date-range").parent().css("display", "none");
+  //$("a.map-multiple-selection").parent().css("display", "none");
 });
