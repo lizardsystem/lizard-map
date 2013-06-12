7ea26ca85dceba4067940749d1b6484a9fb2b19b
Roland van Laar <roland@micite.net>
2013-06-12 10:40:32 +0200

    Remove the second close button.

--- lizard_map/static/lizard_map/lizard_map.js
+++ lizard_map/static/lizard_map/lizard_map.js
@@ -2435,21 +2435,12 @@ function setup_location_search () {
 
 		div_content.empty();
 
-        var $closeBtn = $('<button type="button" class="close">&times;</button>')
-        .on('click', function (event) {
-            div_results.empty();
-            div_results.hide();
-        });
-        var $closeBtnPane = $('<div style="height: 20px;">')
-        .append($closeBtn)
-        .appendTo(div_results);
-
         if (items.length != 0) {
             $('<ul/>', {
                html: items.join('')
             }).appendTo(div_content);
         } else {
-            $('<p>', { html: "Niets gevonden..." }).appendTo('#results');
+            $('<p/>', { html: "Er is niets gevonden." }).appendTo(div_content);
         }
 		div_results.show();
 	}
