41d96dee5e3e1afb7901f885ef99587a8102f21f
Reinout van Rees <reinout@vanrees.org>
2015-10-08 16:18:28 +0200

    The lizard5 locale middleware now optionally looks at user prefs
    If the language picker is shown, we obviously want to look at the language set
    by the user.
    If not, the language_code Setting is looked up. That's a proper Setting with a
    default now, so the try/except could be removed.

--- lizard_map/middleware.py
+++ lizard_map/middleware.py
@@ -2,6 +2,7 @@ import logging
 
 from django.middleware.locale import LocaleMiddleware
 from django.utils import translation
+from django.utils.translation import get_language_from_request
 
 from lizard_map.models import Setting
 
@@ -13,16 +14,15 @@ class LocaleFromSettingMiddleware(LocaleMiddleware):
     """Set language from lizard_map Setting model."""
 
     def process_request(self, request):
-
         language = None
+        if Setting.get('show_language_picker'):
+            # We don't want the regular lizard5 per-site configured language,
+            # we want the user's preferred language instead.
+            language = get_language_from_request(request)
 
-        try:
-            language = Setting.objects.get(key__iexact='language_code').value
-        except Setting.DoesNotExist as err:
-            logger.debug(err)
-            
         if language is None:
-            return
+            # Look for the per-site Setting object.
+            language = Setting.get('language_code')
 
         translation.activate(language)
         request.LANGUAGE_CODE = translation.get_language()
