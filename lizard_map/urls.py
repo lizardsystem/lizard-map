bfe59ca1ac8cfa4f949f97c69559037a8a2e9768
Casper van der Wel <casper.vanderwel@nelen-schuurmans.nl>
2018-04-24 15:19:13 +0200

    Finalize clean views, remove api

--- lizard_map/urls.py
+++ lizard_map/urls.py
@@ -1,15 +1,7 @@
-from django.conf import settings
-from django.conf.urls.defaults import include
 from django.conf.urls.defaults import patterns
-from django.conf.urls.defaults import url
-from django.contrib import admin
-from lizard_ui.urls import debugmode_urlpatterns
-
-import lizard_map.views
 
 urlpatterns = patterns(
     '',
-    (r'^api/', include('lizard_map.api.urls')),
 
     # Load and save map location
     (r'^map_location_save$',
--- lizard_map/views.py
+++ lizard_map/views.py
@@ -3,16 +3,11 @@ try:
 except ImportError:
     import StringIO
 
-import datetime
 import logging
-import re
 
-from django.conf import settings
-from django.core.urlresolvers import reverse
 from django.http import HttpResponse
 from django.utils import simplejson as json
 from django.utils.translation import ugettext as _
-from lizard_ui.layout import Action
 from lizard_ui.views import UiView
 
 from lizard_map.coordinates import DEFAULT_OSM_LAYER_URL
@@ -30,14 +25,9 @@ DEFAULT_PROJECTION = 'EPSG:900913'
 logger = logging.getLogger(__name__)
 
 
-class MapMixin(object):
+class MapView(UiView):
     """All map stuff
     """
-    # def maps(self):
-    #     # Add map variables.
-    #     self.map_variables = map_variables(self.request)
-    #     return ""
-
     def max_extent(self):
         s = Setting.extent(
             'max_extent',
@@ -58,9 +48,6 @@ class MapMixin(object):
     def display_projection(self):
         return Setting.get('projection', 'EPSG:4326')
 
-    def googlemaps_api_key(self):
-        return Setting.get('projection', '')  # Must be defined
-
     def base_layer_name(self):
         if MAP_BASE_LAYER in self.request.session:
             return self.request.session[MAP_BASE_LAYER]
@@ -71,13 +58,6 @@ class MapMixin(object):
         if not hasattr(self, '_backgrounds'):
             self._backgrounds = BackgroundMap.objects.filter(active=True)
         return self._backgrounds
-
-    def has_google(self):
-        # For the client side to determine is there is a google map.
-        if self.backgrounds.filter(
-            layer_type=BackgroundMap.LAYER_TYPE_GOOGLE).count() > 0:
-            return True
-        return False
 
     def background_maps(self):
         if self.backgrounds:
@@ -90,20 +70,6 @@ class MapMixin(object):
                 layer_type=BackgroundMap.LAYER_TYPE_OSM,
                 layer_url=DEFAULT_OSM_LAYER_URL), ]
 
-
-class AppView(MapMixin, UiView):
-    """Main map view (using twitter bootstrap)."""
-
-    show_secondary_sidebar_icon = 'icon-list'
-    map_show_default_zoom = getattr(settings, 'MAP_SHOW_DEFAULT_ZOOM', True)
-    map_show_base_layers_menu = getattr(settings,
-                                        'MAP_SHOW_BASE_LAYERS_MENU', True)
-    map_show_layers_menu = getattr(settings, 'MAP_SHOW_LAYERS_MENU', True)
-
-    @property
-    def show_secondary_sidebar_title(self):
-        return _('Layers')
-
     @property
     def show_rightbar_title(self):
         return _('Legend')
@@ -113,40 +79,6 @@ class AppView(MapMixin, UiView):
         """Return legends for the rightbar."""
         return []  # legends are determined by the blockbox view
 
-    @property
-    def _bak_content_actions(self):
-        """Add default-location-zoom."""
-        actions = super(AppView, self).content_actions
-        if self.map_show_default_zoom and False:
-            zoom_to_default = Action(
-                name='',
-                description=_('Zoom to default location'),
-                url=reverse('lizard_map.map_location_load_default'),
-                icon='icon-screenshot',
-                klass='map-load-default-location')
-            actions.insert(0, zoom_to_default)
-        if self.map_show_base_layers_menu and False:
-            show_layers = Action(
-                name='',
-                element_id='base-layers',
-                description=_('Show base layers'),
-                url="#",
-                icon='icon-globe',
-                klass='dropdown-toggle')
-            actions.insert(0, show_layers)
-        if self.map_show_layers_menu and False:
-            show_layers = Action(
-                name='',
-                element_id='layers',
-                description=_('Show map layers'),
-                url="#",
-                icon='icon-map-marker',
-                klass='dropdown-toggle')
-            actions.insert(0, show_layers)
-        return actions
-
-
-MapView = AppView  # BBB
 
 """
 Map locations are stored in the session with key MAP_SESSION. It
